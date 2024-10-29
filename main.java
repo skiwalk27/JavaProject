e2606dfa85e9f6cd6fb2cd6b25adab3c1583a6bf
skiwalk27 <alexander.walker@uj.edu>
2024-10-29 12:23:30 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,11 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      int players = 0;
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.println("How many players? (1 or 2)");
+      players = scan.nextInt();
+  }
+}
